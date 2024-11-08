03c7713b466093aaac403eebb78146315fac4f5f
ElGuillermo <66552553+ElGuillermo@users.noreply.github.com>
2024-11-08 18:35:02 +0100

    Update restart.sh

--- hll_rcon_tool/restart.sh
+++ hll_rcon_tool/restart.sh
@@ -4,24 +4,28 @@ echo "Build CRCON"
 echo "----------------------------------------"
 cd /root/hll_rcon_tool
 docker compose build
+echo "----------------------------------------"
 echo "Build CRCON : done."
 
 # echo " "
 # echo "Stop CRCON"
 # echo "----------------------------------------"
 # docker compose down
+# echo "----------------------------------------"
 # echo "Stop CRCON : done."
 
 echo " "
 echo "Start CRCON"
 echo "----------------------------------------"
 docker compose up -d --remove-orphans
+echo "----------------------------------------"
 echo "Start CRCON : done."
 
 echo "Clean Docker files"
 echo "----------------------------------------"
 docker volume rm $(docker volume ls -qf dangling=true)
 # docker system prune -a -f
+echo "----------------------------------------"
 echo "Clean Docker files : done."
 
 echo " "
